e0b5e3dbe13c67e1bc5bac56729f474b3e05a2bc
Dmitry Vasilevich <wotlk777@gmail.com>
2018-11-07 03:11:56 +0300

    e, n, eulerFunc. Started Extended Euclidian Algorithm

--- RSA/ViewModel.cs
+++ RSA/ViewModel.cs
@@ -12,7 +12,8 @@ namespace RSA
 {
     public class ViewModel : INotifyPropertyChanged
     {
-        private const int N = 1024; // bits count to generate random primes
+        private const int N = 1024, // bits count to generate random primes
+            e = 65537; // 3, 5, 17, 257 are common choices too
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -23,30 +24,44 @@ namespace RSA
 
         public ViewModel()
         {
-            var r = IsPrime(7451, 128);
-            GetRandomPrime();
+            //var r = IsPrime(7451, 128);
+            var pq = new BigInteger[] { GetRandomPrime(), GetRandomPrime() };
+
+            var n = pq[0] * pq[1];
+            var EulerFunc = (pq[0] - 1) * (pq[1] - 1);
+
+            //{e,n} - public key
+
+            ExtendedEuclidianAlgorithm(e, EulerFunc, out var d, out var ignore);
         }
 
-        private void GetRandomPrime()
+        private BigInteger GetRandomPrime()
         {
             var k = N / 2;
             BigInteger num = 0;
+            var rand = new Random();
 
+            var bits = new BitArray(k);
+
+            string str = "";
+
+            for (int i = 0; i < k; i++)
+            {
+                if (i == 0 || i == 1 || i == 511)
+                    str += "1";
+                else
+                    str += Convert.ToBoolean(rand.Next(0, 2)) ? "1" : "0";
+            }
+
+            num = BinToDec(str) - 2;
             do
             {
-                var rand = new Random();
-                var bits = new BitArray(k);
-
-                string str = "";
-
-                for (int i = 0; i < k; i++)
-                {
-                    str += Convert.ToBoolean(rand.Next(0, 2)) ? "1" : "0";
-                }
-
-                num = BinToDec(str);
+                num += 2;
             }
-            while (!IsPrime(num, 128));
+            // last part of condition is less-expensive test of gcd(num-1, e) == 1, coz e is odd prime
+            while (!IsPrime(num, 128) && (num % e != 1)); 
+
+            return num;
         }
 
         private bool IsPrime(BigInteger num, int accuracy)
@@ -55,9 +70,9 @@ namespace RSA
 
             var d = GetDNumber(num);
 
-            for(int i = 0; i < accuracy; i++)
+            for (int i = 0; i < accuracy; i++)
             {
-                if(!MillerTest(num, d)) return false;
+                if (!MillerTest(num, d)) return false;
             }
 
             return true;
@@ -86,7 +101,7 @@ namespace RSA
 
             if (pow == 1 || pow == n - 1) return true;
 
-            while(d != n - 1)
+            while (d != n - 1)
             {
                 pow = (pow * pow) % n;
                 d = d << 1;
@@ -105,10 +120,10 @@ namespace RSA
             // making the x be less then n
             x %= n;
 
-            while(y > 0)
+            while (y > 0)
             {
                 // if y is odd, multiply x with result
-                if((y & 1L) == 1)
+                if ((y & 1L) == 1)
                 {
                     res = (res * x) % n;
                 }
@@ -132,17 +147,16 @@ namespace RSA
                 random.NextBytes(bytes);
                 bytes[bytes.Length - 1] &= 0x7F; //force sign bit to positive
                 R = new BigInteger(bytes);
-            } while (!(R >=2 && R <= max));
+            } while (!(R >= 2 && R <= max));
 
             return R;
         }
 
         public BigInteger BinToDec(string value)
         {
-            // BigInteger can be found in the System.Numerics dll
             BigInteger res = 0;
 
-            // I'm totally skipping error handling here
+            // "I'm totally skipping error handling here" - пасеба чувак
             foreach (char c in value)
             {
                 res <<= 1;
@@ -150,6 +164,40 @@ namespace RSA
             }
 
             return res;
+        }
+
+        private BigInteger GetGCD(BigInteger a, BigInteger b)
+        {
+            while(b > 0)
+            {
+                var r = a % b;
+                a = b;
+                b = r;
+            }
+
+            return a;
+        }
+
+        private BigInteger ExtendedEuclidianAlgorithm(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+        {
+            x = -1; y = -1;
+
+            if (b == 0)
+            {
+                x = 1; y = 0;
+                return a;
+            }
+
+            int x1 = 0, x2 = 1,
+                y1 = 1, y2 = 0;
+
+            while(b > 0)
+            {
+                var q = Math.Floor(Convert.ToDecimal(a / b));
+
+            }
+
+            return 0;
         }
     }
 }
